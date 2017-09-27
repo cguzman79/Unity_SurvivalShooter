af4f46c88b3f797af008ab7d615cc7d1e590d257
Chris Guzman <cguzman79@users.noreply.github.com>
2017-09-27 10:44:23 -0500

    Add files via upload

---
+++ Scripts/Managers/QuitOnEscape.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOnEscape : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKey(KeyCode.Escape)){
+
+			if (UnityEditor.EditorApplication.isPlaying) {
+				UnityEditor.EditorApplication.isPlaying = false;
+			}
+
+			Application.Quit(); // Quits the game
+		}
+	}
+}
